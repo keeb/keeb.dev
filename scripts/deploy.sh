e0fa2c1c3a8e9b0385bca3f7bf7115017021a38e
keeb <keeb@keeb.dev>
2023-06-25 14:34:11 -0700

    added restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 
+unlink site-active
 docker pull keeb/keeb.dev
 docker run --name tmp-keeb-dev-deploy keeb/keeb.dev echo true 
 # || docker rm tmp-keeb-dev-deploy
 docker cp tmp-keeb-dev-deploy:/hexo/keeb.dev/public .
 docker rm tmp-keeb-dev-deploy
 mv public $(date +%F)
-ln -s $(date +%F) site-current 
+ln -s $(date +%F) site-active
+docker-compose restart
