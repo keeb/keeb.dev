10b11716526958d47b5837baad7a429304ef7127
keeb <keeb@keeb.dev>
2023-07-20 07:43:37 -0700

    updated new post script

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
 docker run -v /home/keeb/code/projects/keeb.dev/overlay/writing:/hexo/keeb.dev/source/_posts \
-    --rm keeb/hexo-new-base \
+    --user 1000:1000 \
+    --rm keeb/keeb.dev \
     hexo new "${*}"
