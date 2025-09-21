0271aa8cf704aa19d21b8ed13a35e122edcd2fc7
keeb <nick@stinemates.org>
2025-09-21 15:00:53 -0700

    fix it up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/sh
 
-# Remove current active site link
-[ -L site-active ] && unlink site-active
-
-# Pull latest image and extract generated static files
+unlink site-active
 docker pull keeb/keeb.dev
-docker run --name tmp-keeb-dev-deploy keeb/keeb.dev echo "deployment ready"
+docker run --name tmp-keeb-dev-deploy keeb/keeb.dev echo true
+# || docker rm tmp-keeb-dev-deploy
 docker cp tmp-keeb-dev-deploy:/app/public .
 docker rm tmp-keeb-dev-deploy
-
-# Create timestamped deployment
 mv public $(date +%F)
 ln -s $(date +%F) site-active
-
-# Restart services if using docker-compose
-if [ -f docker-compose.yml ]; then
-    docker-compose restart
-fi
+docker-compose restart
